0f41a94fe54f4fff072df876afb29ddb35685e54
CiaranJRogers <ciaran.rogers@outlook.com>
2026-03-17 02:00:10 +0000

    Fixing commit issues

---
+++ Core/drivers/pins.h
@@ -0,0 +1,20 @@
+/* Defining main.h to have so there is no recursion*/
+
+#ifndef __PINS_
+#define __PINS_
+#ifdef C_PLUS_PLUS
+C{
+#endif
+
+
+
+/* Pin definitions*/
+#define LED 2
+
+
+
+
+#ifdef C_PLUS_PLUS
+}
+#endif
+#endif
