754b3626f5b2a01946c225266d7e0e110e459790
Aidan McIntosh <aidanmcintosh@outlook.com>
2026-04-02 13:35:18 +0100

    Start class for handling all inputs

---
+++ Core/src/eventHandler.cpp
@@ -0,0 +1,43 @@
+#include "eventHandler.hpp"
+
+EventHandler::EventHandler(EventQueue& queue,
+            std::mutex& queueMutex,
+            std::condition_variable& queueCv)
+      : eventQueue(queue),
+        eventQueueMutex(queueMutex),
+        eventQueueCv(queueCv),
+        bus("/dev/i2c-1"),
+        tofSensor(bus, 0x29, "/dev/gpiochip0", 4)
+{
+}
+
+bool EventHandler::initialise(){
+        if(!tofSensor.initialise()) {
+            return false;
+        }
+
+        tofSensor.registerCallback(
+            [&](uint16_t distance)
+            {
+                handleToFDistance(distance);
+            });
+
+        return true;
+}
+
+void EventHandler::start(){
+    tofSensor.start();
+}
+
+void EventHandler::handleToFDistance(uint16_t distance) {
+    RawInputEvent event{};
+    event.type = RawInputEvent::Type::ToFDistance;
+    event.tofDistance = distance;
+
+    {
+        std::lock_guard<std::mutex> lock(eventQueueMutex);
+        eventQueue.push(event);
+    }
+
+    eventQueueCv.notify_one();
+}
