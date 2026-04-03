06ede51d22acd7e47d715e23db689faac2ac01a0
Aidan McIntosh <aidanmcintosh@outlook.com>
2026-04-03 13:48:50 +0100

    Start simple audio output class

---
+++ Core/include/drivers/audio_output.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <alsa/asoundlib.h>
+#include <cstdint>
+
+class AudioOutput {
+public:
+    AudioOutput();
+    ~AudioOutput();
+
+    void writeSamples(const int16_t* buffer, int frames);
+
+private:
+    snd_pcm_t* handle;
+};
