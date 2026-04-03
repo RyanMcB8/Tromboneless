582fc39c227bb2e02dc8a69491d373a317b078af
RyanMcB8 <ryanmcbride8@icloud.com>
2026-04-04 00:04:41 +0100

    First block of the synth test functions have been completed.

--- Core/tests/testSynth.cpp
+++ Core/tests/testSynth.cpp
@@ -16,6 +16,164 @@
 
 /* ========================================================================================== */
 /*                                                                                            */
+/*                                          Notes                                             */
+/*                                                                                            */
+/* ========================================================================================== */
+bool TestNotes(){
+    /* Creating an instance of the Notes class. */
+    Notes testNote = Notes();
+
+    /* Testing the set/get note. */
+    float noteRead = testNote.getNote(Notes::Notes_t::note_C);
+    if (0 >= noteRead){
+        std::cerr << "[FAIL] Error with getNote() function." << "\n";
+        return false;
+    }
+    
+    testNote.setNote(Notes::Notes_t::note_C, noteRead + 15.0);
+    if (testNote.getNote(Notes::Notes_t::note_C) != (noteRead + 15.0)){
+        std::cerr << "[FAIL] Error with setNote() function." << "\n";
+        return false;
+    }
+    std::cout << "[PASS] Notes passed all tests. \n";
+    return true;
+}
+
+/* ========================================================================================== */
+/*                                                                                            */
+/*                                        Ocatves                                             */
+/*                                                                                            */
+/* ========================================================================================== */
+bool TestOctaves(){
+    /* Creating an instance of the Notes class. */
+    Octaves testOctave = Octaves();
+
+    /* Testing that the octave system is doubling each octave of the same note by default. */
+    if (testOctave.octaves[0].getC() != testOctave.octaves[1].getC() || 
+        testOctave.octaves[1].getC()*2 != testOctave.octaves[2].getC()){
+        std::cerr << "[FAIL] Error with intialising the note frequencies." << "\n";
+        return false;
+    }
+
+    /* Testing that the clamp function works. */
+    if(0 > testOctave.Clamp01(-10) || 1 < testOctave.Clamp01(10)){
+        std::cerr << "[FAIL] Error with Clamp01() function.\nValues returned not between 0 and 1." << "\n";
+        return false;
+    }
+
+    /*  Checking that the TimeAscension function has values increasing as t increases. Proportion doesn't matter. */
+    if (testOctave.TimeAscension(0.1) > testOctave.TimeAscension(0.8)){
+        std::cerr << "[FAIL] Error with TimeAscension() function.\nIncreasing values of t do not return a greater value." << "\n";
+        return false;
+    }
+
+    /*  Testing that the TimeDecay function has values decreasing as t is increased. Proportion doesn't matter. 
+        Saturation values must be kept the same for the comparison. */
+    if (testOctave.TimeDecay(0.1, 0.5) > testOctave.TimeDecay(0.8, 0.5)){
+        std::cerr << "[FAIL] Error with TimeDecay() function.\nIncreasing values of t do not return a lower value." << "\n";
+        return false;
+    }
+
+    /*  Testing that the TimeRest function reaches both its bounds and there is no discontinuity from saturation to rest. */
+    if ((0.5 != testOctave.TimeRest(0.0, 0.5)) || (0 != testOctave.TimeRest(1.0, 0.5))){
+        std::cerr << "[FAIL] Error with TimeRest() function.\nIncreasing values of t do not return a lower value." << "\n";
+        return false;
+    }
+    
+    /*  Testing that the TimeRest function has values which decrease as t increases. */
+    if (testOctave.TimeRest(0.1, 0.5) > testOctave.TimeRest(0.8, 0.5)){
+        std::cerr << "[FAIL] Error with TimeRest() function.\nInitial and/or final values of t do not reach the require amplitude." << "\n";
+        return false;
+    }
+
+    /* Checking that the playing note function can return a cosine wave. */
+    testOctave.octaves[1].setC(1);  /*  Setting this note to have a frequency of 1 for the test. */
+    float noteOutput = 0;
+    for (int i=0; i < 9; i++){
+        noteOutput = testOctave.PlayingNote(1, Notes::Notes_t::note_C, (float)(i/8));
+
+        /* These should be the zero crossings. */
+        if (1 == i%4 || 3 == i%4){
+            if (0.0 != noteOutput){
+                std::cerr << "[FAIL] Error with PlayingNote() function.\nUnexpected value returned - not acting like a cosine wave." << "\n";
+                return false;
+            }
+        }
+
+        /* This should be the peak value. */
+        else if(0 == i%8){
+            if (1.0 != noteOutput){
+                std::cerr << "[FAIL] Error with PlayingNote() function.\nUnexpected value returned - not acting like a cosine wave." << "\n";
+                return false;
+            }
+        }
+
+        /* This should be the trough value. */
+        else if(4 == i%8){
+            if (-1.0 != noteOutput){
+                std::cerr << "[FAIL] Error with PlayingNote() function.\nUnexpected value returned - not acting like a cosine wave." << "\n";
+                return false;
+            }
+        }
+
+        /* This will be the side of the medium values*/
+        else if (1 == i%8 || 7 == i%8){
+            /* Checkling for sqrt(2)/2 allowing for a small error. */
+            if (0.720 < noteOutput || 0.69 > noteOutput){
+                std::cerr << "[FAIL] Error with PlayingNote() function.\nUnexpected value returned - not acting like a cosine wave." << "\n";
+                return false;
+            }
+        }
+
+        /* This will be the negative side of the medium values*/
+        else if (1 == i%8 || 7 == i%8){
+            /* Checkling for sqrt(2)/2 allowing for a small error. */
+            if (-0.720 > noteOutput || -0.69 < noteOutput){
+                std::cerr << "[FAIL] Error with PlayingNote() function.\nUnexpected value returned - not acting like a cosine wave." << "\n";
+                return false;
+            }
+        }
+    }
+
+    /* Checking that the ADSR functions are using the correct function calls within them to determine the amplitude at that point. */
+    for (int i=0; i < 12; i++){
+
+    /*  A */
+        if (testOctave.StartNote(0, Notes::Notes_t::note_C, i, (float)(i/20)) != 
+            testOctave.PlayingNote(0, Notes::Notes_t::note_C, i) * testOctave.TimeAscension((float)(i/20))){
+                std::cerr << "[FAIL] Error with StartNote() function.\nDoes not align with the products of Attack and playing note funcitons." << "\n";
+                return false;
+        }
+    /*  D */
+        if (testOctave.DecayNote(0, Notes::Notes_t::note_C, i, (float)(i/20), 0.5) != 
+            testOctave.PlayingNote(0, Notes::Notes_t::note_C, i) * testOctave.TimeDecay((float)(i/20), 0.5)){
+                std::cerr << "[FAIL] Error with DecayNote() function.\nDoes not align with the products of Decay and playing note funcitons." << "\n";
+                return false;
+        }
+    /*  S */
+        if(testOctave.SaturatedNote(0, Notes::Notes_t::note_C, i, 0.5) !=
+            testOctave.PlayingNote(0, Notes::Notes_t::note_C, i) * 0.5){
+                std::cerr << "[FAIL] Error with SaturatedNote() function.\nDoes not align with the products of saturation and playing note funcitons." << "\n";
+                return false;
+        }
+    /*  R */
+        if(testOctave.EndNote(0, Notes::Notes_t::note_C, i, (float)(i/20), 0.5) !=
+            testOctave.PlayingNote(0, Notes::Notes_t::note_C, i) * testOctave.TimeRest((float)(i/20), 0.5)){
+                std::cerr << "[FAIL] Error with EndNote() function.\nDoes not align with the products of Rest and playing note funcitons." << "\n";
+                return false;
+        }
+
+    }
+
+
+    std::cout << "[PASS] Ocatves passed all tests. \n";
+    return true;
+}
+
+
+
+/* ========================================================================================== */
+/*                                                                                            */
 /*                                      DeltaTime                                             */
 /*                                                                                            */
 /* ========================================================================================== */
@@ -27,10 +185,10 @@ bool TestDeltaTime(){
         return false;
     }
     /* Adding a slight delay to allow the start time to be more than the end time. */
-    std::this_thread::sleep_for(std::chrono::microseconds(100));
+    std::this_thread::sleep_for(std::chrono::microseconds(10));
 
 
-    /* Checking that the */
+    /* Checking that there is a difference in time measured when a short period of time has elapsed. */
     if (0 > deltaTime.getDifference()){
         std::cerr << "[FAIL] Error reading the difference in time in deltaTime" << "\n";
         return false;
@@ -45,12 +203,8 @@ int main() {
 
     /* Calling upon the test funtions to run. */
     success &= TestDeltaTime();
+    success &= TestNotes();
 
 
-
-    if (success) {
-        return 0; 
-    } else {
-        return 1; 
-    }
+    return (success) ? 0 : 1;
 }
