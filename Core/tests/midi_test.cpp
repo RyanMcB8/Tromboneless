90f84164f6dc4200efa337be6b8182535e93ff3c
Aidan McIntosh <aidanmcintosh@outlook.com>
2026-04-07 12:41:26 +0100

    Add simple lidar listener to test script

--- Core/tests/midi_test.cpp
+++ Core/tests/midi_test.cpp
@@ -1,15 +1,28 @@
 
 #include "main.hpp"
+#include <eventHandler.hpp>
+
 #include <condition_variable>
 #include <cstdint>
 #include <iostream>
 #include <thread>
 #include <mutex>
 #include <queue>
-//#include <rtmidi>
-#include <eventHandler.hpp>
+
 #include "MidiCoordinator.hpp"
 #include "tromboneSynth.hpp"
+
+class GetDistance {
+public:
+    int hasTOFsample(uint16_t distance) {
+        if (distance > 500) distance = 500;
+        int scaled_bend = 8192 - (distance * 8192) / 500;
+        std::cout << scaled_bend << "\n";
+        return scaled_bend;
+    }
+};
+
+char key;
 
 int main(){
 try
