b5930c96f0da098a02b95af78cae9fe10baaa955
RyanMcB8 <ryanmcbride8@icloud.com>
2026-04-18 21:56:43 +0100

    Updated main to use the wrapper.

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -16,96 +16,11 @@
 
 int main() {
     try {
-        std::queue<RawInputEvent> eventQueue;
-        std::mutex eventQueueMutex;
-        std::condition_variable eventQueueCv;
+        CoreWrapper coreWrapper;
+        coreWrapper.start();
 
-        EventHandler eventHandler(eventQueue, eventQueueMutex, eventQueueCv);
-        RtMidiSink midiSink;
-        AudioRender render;
-        AmplitudeMapper amplitudemapper;
-        PitchMapper pitchmapper;
-        MidiCoordinator coordinator(render);
-        
-        const bool externalDevicePresent = midiSink.GetDeviceStatus();
-        coordinator.setDevice(externalDevicePresent);
-
-        if (!eventHandler.initialise()) {
-            std::cerr << "Initialisation failed\n";
-            return 1;
-        }
-
-        coordinator.RegisterCallback(
-            [&](const MidiMessage& msg) {
-                midiSink.send(msg);
-            });
-
-        eventHandler.start();
-
-        if (!externalDevicePresent) {
-            render.start();
-
-            render.setDebugTone(true);
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-            render.setDebugTone(false);
-
-            std::cout << "Internal synth mode\n";
-        } else {
-            std::cout << "External MIDI mode\n";
-        }
-
-        std::cout << "Beginning Pressure Baseline Calculation\n";
-        // Baseline collection loop
-        while (true) {
-            RawInputEvent event;
-            {
-                std::unique_lock<std::mutex> lock(eventQueueMutex);
-                eventQueueCv.wait(lock, [&] { return !eventQueue.empty(); });
-                event = eventQueue.front();
-                eventQueue.pop();
-            }
-
-            // Only feed pressure readings to the baseline
-            if (event.type == RawInputEvent::Type::PressureReading) {
-                if (amplitudemapper.calculateBaseline(event.pressureReading)) {
-                    break;
-                }
-            }
-        }
-        std::cout << "Pressure Baseline found to be: " << amplitudemapper.getBaseline() << "\n";
-
-
-        // bool pressure_gate = false;
-        int current_note = 0;
-        int new_note = 0;
-
-        while (true) {
-            RawInputEvent event;
-            {
-                std::unique_lock<std::mutex> lock(eventQueueMutex);
-                eventQueueCv.wait(lock, [&] { return !eventQueue.empty(); });
-                event = eventQueue.front();
-                eventQueue.pop();
-            }
-
-            switch (event.type) {
-                case RawInputEvent::Type::ToFDistance:
-                    coordinator.setBend(pitchmapper.tof_to_MIDI_bend(event.tofDistance));
-                    break;
-
-                case RawInputEvent::Type::PressureReading:
-                    coordinator.PressureEdge(amplitudemapper.noteEdge(event.pressureReading));
-                    break;
-
-                case RawInputEvent::Type::MouthpieceReading:
-                    new_note = pitchmapper.mouthpiece_to_MIDI_note(event.mouthpieceReading);
-                    if (current_note != new_note && new_note != 0) {
-                        current_note = new_note;
-                        coordinator.ChangeNote(current_note);
-                    }
-                    break;
-            }
-        }
+        /* Runnning core indefinitely. */
+        std::this_thread::sleep_for(std::chrono::hours(24));
     }
     catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << "\n";
