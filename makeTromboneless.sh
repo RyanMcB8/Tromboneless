a9798d6beb2086227a5d0ac3165f17b0595b9d21
RyanMcB8 <ryanmcbride8@icloud.com>
2026-04-20 14:42:10 +0100

    Adding a bash file to make the entire project and download dependencies.

---
+++ makeTromboneless.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e  # exit on error
+
+#   List of dependent packages
+packages=(
+  build-essential
+  cmake
+  libssl-dev
+  libcurl4-openssl-dev
+  libgpiod-dev
+  pkg-config
+  libgtk-3-dev
+  libwebkit2gtk-4.1-dev
+  libcurl4-openssl-dev
+)
+
+#   Ensuring that all the necessary dependecies have been installed.
+echo "Installing dependencies..."
+sudo apt update
+sudo apt install -y "${packages[@]}"
+
+echo "Configuring project..."
+#   Removing the previous CMakeCache.txt
+# rm -rf build/CMakeCache.txt
+cmake -S . -B build -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Release
+
+echo "Building..."
+cmake --build build
+
+echo "Done."
+
+#   Asking the user if they wish to run the exectuable after it has been built.
+read -p "Run the executable now? [y/N]: " answer
+case "$answer" in
+  ([yY][eE][sS]|[yY])
+    ./build/Tromboneless
+    ;;
+  (*)
+    echo "Skipping execution."
+    ;;
+esac
