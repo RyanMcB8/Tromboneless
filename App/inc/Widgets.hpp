3714d47360f3f587617a18d1ebc029f713458658
RyanMcB8 <ryanmcbride8@icloud.com>
2026-04-20 15:07:48 +0100

    Adjusted Barometer

--- App/inc/Widgets.hpp
+++ App/inc/Widgets.hpp
@@ -387,10 +387,10 @@ private:
     /*  Setting the minimum limit of the slider. */
     float minLimit = 0.1;
     /*  Setting the maximum limit of the slider. */
-    float maxLimit = 3.0;
+    float maxLimit = 2.5;
 
     /*  Setting the minimum step to which the slider may be moved. */
-    float interval = 0.01;
+    float interval = 0.05;
 
     /* This is the minimum difference between the min and max sliders with respect ot the non normalised values. */
     float minDifference = 0.01;
